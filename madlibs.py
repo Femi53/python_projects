903b115423ed8a1168f59eddc9f6db37e0efee2a
Oluwafemi Babalola <oluwafemibabalolae@gmail.com>
2022-07-29 11:25:20 +0100

    Playing with string concatenation to create this madlibs game mockup

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# # STRING CONCATENATION (aka how to put strings together)
+
+# # suppose we want to create a string that says "subscribe to ________" 
+# blogger = "femi babs"    #some string
+
+# # Here are a few ways to do this
+# print("subscribe to " + blogger)
+# print("subscribe to {}".format(blogger))
+# print(f"subscribe to {blogger}")
+
+# MADLIB PROPER
+noun = input("Animal: ")
+madlib1 = f"Be careful not to kill the {noun} when you shoot in the jungle."
+
+print(madlib1)
+
---
+++ madlibs1.py
@@ -0,0 +1,21 @@
+# This is a more work on madlibs. I hope you have fun playing.
+
+feeling = input("Emotion: ")
+food = input("Favorite meal: ")
+love = input("Love gesture: ")
+love1 = input("Group love gesture: ")
+color = input("Valentine color: ")
+feeling1 = input("Meaning of given color: ")
+adj = input("adjective: ")
+holiday = input("favorite holiday: ")
+animal1 = input("pet: ")
+animal2 = input("pet: ")
+affection = input("love gesture : ")
+
+madlib = f"""Valentine's day is a great day to show all your family and friends how much you {feeling} them. \
+You can make {food} for your mom, give a {love} to your dad, and give everyone you love a big {love1} \n 
+It can be fun to wear {color} on valentine's day because it is the color of {feeling1}. Don't forget your \
+{adj} friends on {holiday} either. Do not forget; even {animal1} and {animal2} like hugs and {affection}"""
+
+print(madlib)
+
